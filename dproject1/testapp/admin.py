d05587a9bad87f6ccf935c2c87c975975cbf4ef9
Kuncha Vamshi Reddy <vamshireddy594@gmail.com>
2025-04-22 20:56:31 +0530

    1st Commit

--- dproject1/testapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from testapp import models
-# Register your models here.
-class studentadmin(admin.ModelAdmin):
-    list_display=['Rollno','Name','Email','PhoneNo']
-admin.site.register(models.student,studentadmin)
--- dproject1/testapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class student(models.Model):
-    Rollno=models.IntegerField()
-    Name=models.CharField(max_length=40)
-    Email=models.EmailField()
-    PhoneNo=models.IntegerField()
-    
